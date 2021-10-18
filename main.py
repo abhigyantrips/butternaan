31d084b5cf63c822284dc6a5fae320a0bbaae818
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-10-18 19:46:56 +0530

    Minor changes.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import disnake
 from disnake.ext import commands
-from disnake.utils import get
 
 import dotenv
 dotenv.load_dotenv()
@@ -22,8 +21,8 @@ for filename in os.listdir('./basic-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'basic-cmds.{filename[:-3]}')
-            print(f"|  {filename} loaded.")
-        elif filename == "__pycache__":
+            print(f"|  Loaded {filename}.")
+        else:
             pass
     except Exception as e:
         print(f'|  Could not load {filename} due to exception: \n{e}')
@@ -33,8 +32,8 @@ for filename in os.listdir('./cotxt-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'cotxt-cmds.{filename[:-3]}')
-            print(f"|  {filename} loaded.")
-        elif filename == "__pycache__":
+            print(f"|  Loaded {filename}.")
+        else:
             pass
     except Exception as e:
         print(f'|  Could not load {filename} due to exception: \n{e}')
@@ -44,61 +43,22 @@ for filename in os.listdir('./slash-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'slash-cmds.{filename[:-3]}')
-            print(f"|  {filename} loaded.")
-        elif filename == "__pycache__":
+            print(f"|  Loaded {filename}.")
+        else:
             pass
     except Exception as e:
         print(f'|  Could not load {filename} due to exception: \n{e}')
 
-### LOG EVENTS ###
-
-@client.event
-async def on_message_edit(before, after):
-
-    if disnake.errors.HTTPException:
-        pass # Some weird things happen with bots sending embeds, so.
-
-    embed = disnake.Embed(
-    title = "Message Edited.",
-    color = 0x303136,
-    description = f"A message by {before.author.mention} was edited in {before.channel.mention}."
-    )
-
-    embed.set_thumbnail(url = before.author.avatar)
-
-    embed.add_field(name = "Before", value = before.content, inline = False)
-    embed.add_field(name = "After", value = after.content, inline = False)
-
-    await client.get_channel(891527413866053653).send(embed = embed) # The ID refers to the server log in my server.
-
-@client.event
-async def on_message_delete(message):
-
-    async for entry in message.guild.audit_logs(limit = 1, action = disnake.AuditLogAction.message_delete):
-        deleter = entry.user
-
-    if deleter == message.author:
-        desc = f"{message.author.mention} deleted their message in {message.channel.mention}."
-    elif deleter == client.user:
-        pass
-    else:
-        desc = f"A message by {message.author.mention} was deleted in {message.channel.mention} by {deleter.mention}."
-
-    embed = disnake.Embed(
-        title = "Message Deletion.",
-        color = 0x303136,
-        description = desc
-    )
-
-    embed.set_thumbnail(url = message.author.avatar)
-
-    if message.reference is not None and not message.is_system:
-        ref_msg = client.get_message(message.reference.id)
-        embed.add_field(name = f"Replying to {ref_msg.author.mention}", value = ref_msg.content, inline = False)
-    
-    embed.add_field(name = f"Deleted Message", value = message.content, inline = False)
-
-    await client.get_channel(891527413866053653).send(embed = embed) # The ID refers to the server log in my server.
+print("---------- UTILS COMMANDS ----------")
+for filename in os.listdir('./utils-cmds'):
+    try:
+        if filename.endswith('.py'):
+            client.load_extension(f'utils-cmds.{filename[:-3]}')
+            print(f"|  Loaded {filename}.")
+        else:
+            pass
+    except Exception as e:
+        print(f'|  Could not load {filename} due to exception: \n{e}')
 
 ### COG COMMANDS ###
 
@@ -129,8 +89,8 @@ async def reloadall(ctx):
         try:
             if filename.endswith('.py'):
                 client.reload_extension(f'basic-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  {filename} reloaded.**")
-            elif filename == "__pycache__":
+                await ctx.send(f"**|  Reloaded {filename}.**")
+            else:
                 pass
         except Exception as e:
             await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
@@ -140,8 +100,8 @@ async def reloadall(ctx):
         try:
             if filename.endswith('.py'):
                 client.reload_extension(f'cotxt-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  {filename} reloaded.**")
-            elif filename == "__pycache__":
+                await ctx.send(f"**|  Reloaded {filename}.**")
+            else:
                 pass
         except Exception as e:
             await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
@@ -151,8 +111,18 @@ async def reloadall(ctx):
         try:
             if filename.endswith('.py'):
                 client.reload_extension(f'slash-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  {filename} reloaded.**")
-            elif filename == "__pycache__":
+                await ctx.send(f"**|  Reloaded {filename}.**")
+            else:
+                pass
+        except Exception as e:
+            await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
+    await ctx.send("---------- UTILS COMMANDS ----------")
+    for filename in os.listdir('./utils-cmds'):
+        try:
+            if filename.endswith('.py'):
+                client.reload_extension(f'utils-cmds.{filename[:-3]}')
+                await ctx.send(f"**|  Reloaded {filename}.**")
+            else:
                 pass
         except Exception as e:
             await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
