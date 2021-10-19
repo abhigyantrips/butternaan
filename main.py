691a3ed07f6e438ae4effabcb9de8f2c4026c033
Abhigyan Tripathi <abhigyantrips@gmail.com>
2021-10-19 14:39:34 +0530

    Shifted cog-loading commands.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import random
 import asyncio
 
 client = commands.Bot(command_prefix = "!", intents = disnake.Intents().all(), strip_after_prefix = True, test_guilds = [int(os.environ.get('TEST_GUILDS'))])
+client.loadedcogs = []
+client.unloadedcogs = []
 
 @client.event
 async def on_ready():
@@ -21,6 +23,7 @@ for filename in os.listdir('./basic-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'basic-cmds.{filename[:-3]}')
+            client.loadedcogs.append(filename[:-3])
             print(f"|  Loaded {filename}.")
         else:
             pass
@@ -32,6 +35,7 @@ for filename in os.listdir('./cotxt-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'cotxt-cmds.{filename[:-3]}')
+            client.loadedcogs.append(filename[:-3])
             print(f"|  Loaded {filename}.")
         else:
             pass
@@ -43,6 +47,7 @@ for filename in os.listdir('./slash-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'slash-cmds.{filename[:-3]}')
+            client.loadedcogs.append(filename[:-3])
             print(f"|  Loaded {filename}.")
         else:
             pass
@@ -54,80 +59,12 @@ for filename in os.listdir('./utils-cmds'):
     try:
         if filename.endswith('.py'):
             client.load_extension(f'utils-cmds.{filename[:-3]}')
+            client.loadedcogs.append(filename[:-3])
             print(f"|  Loaded {filename}.")
         else:
             pass
     except Exception as e:
         print(f'|  Could not load {filename} due to exception: \n{e}')
-
-### COG COMMANDS ###
-
-@client.command()
-async def load(ctx, extension_folder, extension):
-    client.load_extension(f'{extension_folder}.{extension}')
-    await ctx.send(f'{extension} is now loaded.')
-
-@client.command()
-async def unload(ctx, extension_folder, extension):
-    client.unload_extension(f'{extension_folder}.{extension}')
-    await ctx.send(f'{extension} is now unloaded.')
-
-@client.command()
-async def reload(ctx, extension):
-    if "slash-cmd" in extension:
-        client.reload_extension(f'slash-cmds.{extension}')
-        await ctx.send(f"**{extension}** has been reloaded.")
-    elif "basic-cmd" in extension:
-        client.reload_extension(f'basic-cmds.{extension}')
-        await ctx.send(f"**{extension}** has been reloaded.")
-
-@client.command()
-async def reloadall(ctx):
-
-    await ctx.send("**---------- BASIC COMMANDS ----------**")
-    for filename in os.listdir('./basic-cmds'):
-        try:
-            if filename.endswith('.py'):
-                client.reload_extension(f'basic-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  Reloaded {filename}.**")
-            else:
-                pass
-        except Exception as e:
-            await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
-
-    await ctx.send("**--------- CONTEXT COMMANDS ---------**")
-    for filename in os.listdir('./cotxt-cmds'):
-        try:
-            if filename.endswith('.py'):
-                client.reload_extension(f'cotxt-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  Reloaded {filename}.**")
-            else:
-                pass
-        except Exception as e:
-            await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
-
-    await ctx.send("**---------- SLASH COMMANDS ----------**")
-    for filename in os.listdir('./slash-cmds'):
-        try:
-            if filename.endswith('.py'):
-                client.reload_extension(f'slash-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  Reloaded {filename}.**")
-            else:
-                pass
-        except Exception as e:
-            await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
-    await ctx.send("---------- UTILS COMMANDS ----------")
-    for filename in os.listdir('./utils-cmds'):
-        try:
-            if filename.endswith('.py'):
-                client.reload_extension(f'utils-cmds.{filename[:-3]}')
-                await ctx.send(f"**|  Reloaded {filename}.**")
-            else:
-                pass
-        except Exception as e:
-            await ctx.send(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
-    await ctx.send("**-----------------------------------------------**")
-    await ctx.send("**COGS RELOADED.**")
 
 DISCORD_BOT_TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
 client.run(DISCORD_BOT_TOKEN)
--- utils-cmds/utils-cmd-cogs.py
+++ utils-cmds/utils-cmd-cogs.py
@@ -2,11 +2,149 @@ import disnake
 from disnake import Option, OptionType, OptionChoice, ApplicationCommandInteraction
 from disnake.ext import commands
 
-class CogLoad(commands.Cog):
+import os
+
+class Cogs(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    async def autocomp_loadedcogs(ctx: ApplicationCommandInteraction, user_input: str):
+        return [cog for cog in ctx.client.loadedcogs if (user_input.lower() in cog) or (user_input in cog)]
     
+    async def autocomp_unloadedcogs(ctx: ApplicationCommandInteraction, user_input: str):
+        return [cog for cog in ctx.client.unloadedcogs if (user_input.lower() in cog) or (user_input in cog)]
+    
+    @commands.slash_command(name = 'cog-utils')
+    async def cogutils(self, ctx: ApplicationCommandInteraction):
+        pass
+
+    @cogutils.sub_command(
+        name = 'load',
+        description = 'Load a cog/extension (autocomplete options).'
+    )
+    async def cogload(
+        self, ctx: ApplicationCommandInteraction,
+        cog: str = commands.Param(desc = 'Enter the cog/extension to be loaded into the bot.', autocomp = autocomp_unloadedcogs)
+    ):
+
+        if 'basic' in cog:
+            ctx.client.load_extension(f'basic-cmds.{cog}')
+        elif 'slash' in cog:
+            ctx.client.load_extension(f'slash-cmds.{cog}')
+        elif 'cotxt' in cog:
+            ctx.client.load_extension(f'cotxt-cmds.{cog}')
+        elif 'utils' in cog:
+            ctx.client.load_extension(f'utils-cmds.{cog}')
+        
+        ctx.client.loadedcogs.append(cog)
+        ctx.client.unloadedcogs.remove(cog)
+        
+        await ctx.response.send_message(f'Loaded `{cog}`.')
+
+    @cogutils.sub_command(
+        name = 'unload',
+        description = 'Unload a cog/extension (autocomplete options).'
+    )
+    async def cogunload(
+        self, ctx: ApplicationCommandInteraction,
+        cog: str = commands.Param(desc = 'Enter the cog/extension to be unloaded from the bot.', autocomp = autocomp_loadedcogs)
+    ):
+
+        if 'basic' in cog:
+            ctx.client.unload_extension(f'basic-cmds.{cog}')
+        elif 'slash' in cog:
+            ctx.client.unload_extension(f'slash-cmds.{cog}')
+        elif 'cotxt' in cog:
+            ctx.client.unload_extension(f'cotxt-cmds.{cog}')
+        elif 'utils' in cog:
+            ctx.client.unload_extension(f'utils-cmds.{cog}')
+
+        ctx.client.loadedcogs.remove(cog)
+        ctx.client.unloadedcogs.append(cog)
+        
+        await ctx.response.send_message(f'Unloaded `{cog}`.')
+
+
+    @cogutils.sub_command(
+        name = 'reload',
+        description = 'Reload a cog/extension (autocomplete options).'
+    )
+    async def cogreload(
+        self, ctx: ApplicationCommandInteraction,
+        cog: str = commands.Param(desc = 'Enter the cog/extension to be reloaded into the bot.', autocomp = autocomp_loadedcogs)
+    ):
+
+        if 'basic' in cog:
+            ctx.client.load_extension(f'basic-cmds.{cog}')
+        elif 'slash' in cog:
+            ctx.client.load_extension(f'slash-cmds.{cog}')
+        elif 'cotxt' in cog:
+            ctx.client.load_extension(f'cotxt-cmds.{cog}')
+        elif 'utils' in cog:
+            ctx.client.load_extension(f'utils-cmds.{cog}')
+        
+        await ctx.response.send_message(f'Reloaded `{cog}`.')
+    
+    @cogutils.sub_command(
+        name = 'reloadall',
+        description = 'Reloads all cogs/extensions.'
+    )
+    async def cogreloadall(
+        self, ctx: ApplicationCommandInteraction
+    ):
+
+        await ctx.response.send_message('**Reloading cogs in process...**')
+
+        edit = []
+        edit.append('**---------- BASIC COMMANDS ----------**')
+        for filename in os.listdir('./basic-cmds'):
+            try:
+                if filename.endswith('.py'):
+                    ctx.client.reload_extension(f'basic-cmds.{filename[:-3]}')
+                    edit.append(f'**|  Reloaded {filename}.**')
+                else:
+                    pass
+            except Exception as e:
+                edit.append(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
+
+        edit.append('**--------- CONTEXT COMMANDS ---------**')
+        for filename in os.listdir('./cotxt-cmds'):
+            try:
+                if filename.endswith('.py'):
+                    ctx.client.reload_extension(f'cotxt-cmds.{filename[:-3]}')
+                    edit.append(f'**|  Reloaded {filename}.**')
+                else:
+                    pass
+            except Exception as e:
+                edit.append(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
+
+        edit.append('**---------- SLASH COMMANDS ----------**')
+        for filename in os.listdir('./slash-cmds'):
+            try:
+                if filename.endswith('.py'):
+                    ctx.client.reload_extension(f'slash-cmds.{filename[:-3]}')
+                    edit.append(f'**|  Reloaded {filename}.**')
+                else:
+                    pass
+            except Exception as e:
+                edit.append(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
+
+        edit.append('**---------- UTILS COMMANDS ----------**')
+        for filename in os.listdir('./utils-cmds'):
+            try:
+                if filename.endswith('.py'):
+                    ctx.client.reload_extension(f'utils-cmds.{filename[:-3]}')
+                    edit.append(f'**|  Reloaded {filename}.**')
+                else:
+                    pass
+            except Exception as e:
+                edit.append(f'**|  Could not reload {filename} due to exception:** \n```{e}```')
+        edit.append('**-----------------------------------------------**')
+        edit.append('**COGS RELOADED.**')
+
+        edited = '\n'.join(edit)
+        await ctx.edit_original_message(content = edited)
 
 def setup(client):
-    client.add_cog(CogLoad(client))
+    client.add_cog(Cogs(client))
