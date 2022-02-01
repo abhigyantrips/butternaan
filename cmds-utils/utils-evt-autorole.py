421cc57fc4d3fd58a8ee6b378fa61f81e5134a45
Abhigyan Tripathi <abhigyantrips@gmail.com>
2022-02-01 21:46:48 +0530

    feat(utils): Add autorole after screening completion.

--- cmds-utils/utils-evt-autorole.py
+++ cmds-utils/utils-evt-autorole.py
@@ -17,7 +17,16 @@ class AutoRole(commands.Cog):
         if member.bot:
             bot_role = get(member.guild.roles, name="◍ • Bots")
             await member.add_roles(bot_role)
+    
+    @commands.Cog.listener()
+    async def on_member_update(self, before: disnake.Member, after: disnake.Member):
 
+        if (before.guild.id != os.environ.get("TEST_GUILDS_ONE")) or (before.bot):
+            return
+
+        if before.pending and not after.pending:
+            member_role = get(after.guild.roles, name="▧ • Members")
+            await after.add_roles(member_role)
 
 
 def setup(client):
